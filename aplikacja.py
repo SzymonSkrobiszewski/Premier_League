6a5bcbf2f7ec6043c2ca7c31ee82328ab7cab12d
Skrobek83 <100147724+SzymonSkrobiszewski@users.noreply.github.com>
2023-05-15 11:35:14 +0200

    Update aplikacja.py

--- aplikacja.py
+++ aplikacja.py
@@ -223,7 +223,7 @@ elif selected_tab == "Premier League":
         )
     fig0.update_layout(
         xaxis_title="Sezon",
-        margin=dict(l=10, r=50, t=15, b=50),
+        margin=dict(l=0, r=50, t=15, b=50),
         yaxis=dict(
             range=[0, 8000],
             tickfont=dict(size=13, color='black'),
