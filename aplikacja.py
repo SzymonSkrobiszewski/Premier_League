35ac2f25af1f3e6ca136727ec73a89cac824c9aa
Skrobek83 <100147724+SzymonSkrobiszewski@users.noreply.github.com>
2023-08-21 17:02:44 +0200

    Update aplikacja.py

--- aplikacja.py
+++ aplikacja.py
@@ -1817,9 +1817,9 @@ elif selected_tab == "Drużyny":
                                 conceded_and_scored_goals['GSWDP'].iloc[i],
                             ],
                             marker=dict(
-                                color='blue',
+                                color=hex,
                                 pattern_shape=symbols[number_of_colors_used[color] - 1],
-                                pattern_bgcolor=hex
+                                pattern_bgcolor=color
                             ),
                             text=[
                                 conceded_and_scored_goals['GSWPP'].iloc[i],
